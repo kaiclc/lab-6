ec268c29778a2942c1e385933e49cb46d43a56ea
Jonathan Groberg <jongroberg@gmail.com>
2024-03-18 17:02:12 -0400

    added decode string method

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ def encode(num_string):
         encoded_string += str(new_digit)
     return encoded_string
 
+# Jonathan Groberg
+# Decodes the encoded string
+def decode(encoded_string):
+    decoded_string = ""
+    for digit in encoded_string:
+        new_digit = int(digit)-3
+
+        # if the number goes below 0, add 10 (-3+10 = 7) this is inverse of 7+3 =10 (mod 10 is 0)
+        new_digit = 10+new_digit if new_digit < 0 else new_digit
+        decoded_string += str(new_digit)
+    return decoded_string
+
 
 def main():
     stored = ""
@@ -26,7 +38,8 @@ def main():
             print("Your password has been encoded and stored!")
         elif selection == "2":
             # work in progress!
-            print(f"The encoded password is {stored}")
+            print(
+                f"The encoded password is {stored}, and the original password is {decode(stored)}'")
         elif selection == "3":
             break
 
